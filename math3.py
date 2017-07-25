cc78beb3be4fdd9a86d310cc2c22f64b975d95a8
wangicheng <r2005.wang@gmail.com>
2017-07-25 23:05:24 +0800

    New

--- math3.py
+++ math3.py
@@ -1,11 +1,14 @@
 pi="3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862"
+#pi="3.1415926535"
 f=[]
-for i in range(1,len(pi)-2):
+for i in range(len(pi)-2):
     f.append(pi[2+i:4+i])
 f.sort()
-g=[]
-n=[]
+g=""
+n=0
 for x in range(100,200):
-    g.append((str(x))[1:])
-    n.append(f.count((str(x))[1:]))
-print(dict(zip((g), (n))))
+    g=str(x)[1:3]
+    n=f.count((str(x))[1:])
+    ff=dict()
+    ff[g]=n
+    print(ff)
